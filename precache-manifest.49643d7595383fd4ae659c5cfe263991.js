9f0df295775b1c23f91f9f0733a0f6bb70ba4039
Md Sahadat <sakib2ripat1@gmail.com>
2020-08-15 14:04:20 +0600

    Updates

---
+++ precache-manifest.49643d7595383fd4ae659c5cfe263991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2707cfc996d1b2059ca2fe169d576c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ccab27e0f469ce8c45",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fa3c3ac1df7d34457542",
+    "url": "/static/css/main.f2b95a07.chunk.css"
+  },
+  {
+    "revision": "97ccab27e0f469ce8c45",
+    "url": "/static/js/2.cc5ced67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cc5ced67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3c3ac1df7d34457542",
+    "url": "/static/js/main.a339629b.chunk.js"
+  },
+  {
+    "revision": "ab4404e4bd51e3ec50a0",
+    "url": "/static/js/runtime-main.503c16b0.js"
+  }
+]);
